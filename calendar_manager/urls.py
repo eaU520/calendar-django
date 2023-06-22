7f6043b38e9de4d3e8b40af218ddedcb4778c5e2
eaU520 <ecallowayllc@gmail.com>
2023-06-22 19:46:39 -0400

    Fixed homepage URL

--- calendar_manager/urls.py
+++ calendar_manager/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
+    path('', include('events.urls')),
     path('admin/', admin.site.urls),
     path('events/',include('events.urls')),
     # #register
